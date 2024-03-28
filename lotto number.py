f7bbd8b1fb09833bdb40292aa4068b1ec5894e46
djdbswjd2 <164146069+djdbswjd2@users.noreply.github.com>
2024-03-28 15:58:14 +0900

    Add files via upload

---
+++ lotto number.py
@@ -0,0 +1,19 @@
+import random
+
+random_integer = random.randint(1,45)
+print(random_integer)
+
+random_integer = random.randint(1,45)
+print(random_integer)
+
+random_integer = random.randint(1,45)
+print(random_integer)
+
+random_integer = random.randint(1,45)
+print(random_integer)
+
+random_integer = random.randint(1,45)
+print(random_integer)
+
+random_integer = random.randint(1,45)
+print(random_integer)
